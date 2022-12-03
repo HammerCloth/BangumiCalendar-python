18169ff104b11566230a27481f85a787b4f2057b
siyixiong <18552541076@163.com>
2022-12-03 20:28:11 +0800

    测试

--- data/bangumi.tv/reptile.py
+++ data/bangumi.tv/reptile.py
@@ -1,15 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+from icalendar import Calendar,Event
 
 url = "http://bangumi.tv/calendar"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
-    ,
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
     "Cookie": "chii_sid=H1LI6j; chii_sec_id=IzYtsAZndkyJf3CWdSC7%2BlxuGw%2FFeur7spG9SEg; chii_theme=dark; __utma=1.1194694437.1669987956.1669987956.1669987956.1; __utmc=1; __utmz=1.1669987956.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=1.7.10.1669987956"
 }
-
-page = requests.get(url=url, headers=headers)
+# todo 欠缺封装
+# todo 超时重试机制
+page = requests.get(url=url, headers=headers, )
 page.encoding = 'utf-8'
 content = page.content
 soup = BeautifulSoup(content, "lxml")
@@ -31,6 +32,8 @@ for key in calendar.keys():
         list.append(i.select_one("a").get_text() + "/" + i.select_one("em").get_text())
     calendar[key] = list
 
+# 定制时间，目前先这样认为
 
-
+# 构建ics文件
+cal = Calendar()
 
