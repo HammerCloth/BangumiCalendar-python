e178b17417de43cfc34954cbecd6fa93d1a53816
siyixiong <18552541076@163.com>
2022-12-06 18:18:32 +0800

    版本迭代，采用番组计划api

--- bangumi.tv.v2/iCal.py
+++ bangumi.tv.v2/iCal.py
@@ -5,7 +5,7 @@ from icalendar import Calendar, Event
 
 class iCal:
     def __init__(self):
-        g = open("../../ical/BEGIN:VCALENDAR.ics", "rb")
+        g = open("./ical/BEGIN:VCALENDAR.ics", "rb")
         self.cal = Calendar.from_ical(g.read())
 
     def display(self):
@@ -20,13 +20,16 @@ class iCal:
         event.add('class', 'PUBLIC')
         event.add('summary', summary)
         event.add("TRANSP", "TRANSPARENT")
-        event.add("descripion", descripion)
+        event.add("description", descripion)
         event.add("X-APPLE-UNIVERSAL-ID", "42902458-1dd4-5105-04d0-2dccc0194c5f")
         self.cal.add_component(event)
         return self
 
     def write(self):
-        f = open("../../ical/target.ics", "wb")
+        f = open("./ical/target.ics", "wb")
         f.write(self.cal.to_ical())
         f.close()
         return self
+
+if __name__ == '__main__':
+    iCal().display().write()
